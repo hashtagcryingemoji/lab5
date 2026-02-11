003b471c5b7aef2e0592d32548d3d2f1191c8363
dmchq <gdima617@gmail.com>
2026-02-12 00:07:00 +0300

    feat(domain): implement Comparable interface for Organization

--- src/main/kotlin/domain/Organization.kt
+++ src/main/kotlin/domain/Organization.kt
@@ -12,11 +12,14 @@ class Organization (
     val employeesCount: Long?,
     val type: OrganizationType,
     val officialAddress: Address
-){
+) : Comparable<Organization> {
     init {
         require(id > 0) { "Значение поля должно быть больше 0" }
         require(name != "") { "Строка не может быть пустой" }
         require(annualTurnover > 0) { "Значение поля должно быть больше 0" }
         if (employeesCount != null ) require(employeesCount > 0) { "Значение поля должно быть больше 0" }
+    }
+    override fun compareTo(other: Organization): Int {
+        return id.compareTo(other.id)
     }
 }
