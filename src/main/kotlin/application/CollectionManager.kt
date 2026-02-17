2830d2b0f9568c4ac442f87d8537a340f5c2ff0a
dmchq <gdima617@gmail.com>
2026-02-17 19:24:30 +0300

    feature(domain): add id management to CollectionManager fix(domain): ConcurrentModificationException while iterating and removing elements over collection

--- src/main/kotlin/application/CollectionManager.kt
+++ src/main/kotlin/application/CollectionManager.kt
@@ -8,12 +8,25 @@ import java.util.Deque
 
 class CollectionManager(
     private val organizationCollection : ArrayDeque<Organization>,
-
+    private var currentID: Int = -1
 ) : OrganizationRepository {
 
-    fun clear() = organizationCollection.clear()
+    fun generateNewID() : Int{
+        if (currentID == -1) {
+            if (organizationCollection.isEmpty()) {currentID = 1; return 1}
+            val maxID = organizationCollection.maxOf { it.id }
+            currentID = maxID + 1
+            return maxID + 1
+        }
 
-    fun getCollection() : ArrayDeque<Organization> = organizationCollection
+        return currentID+1
+    }
+
+    fun clear() {
+        organizationCollection.clear()
+        currentID = -1
+    }
+    fun getCollection() : List<Organization> = organizationCollection.toList()
 
     fun countType(type: OrganizationType) : Int = organizationCollection.count {it.type == type}
 
@@ -21,21 +34,9 @@ class CollectionManager(
 
     fun countLessAddress(address: Address): Int = organizationCollection.count { it.officialAddress < address }
 
-    fun removeGreater(organization: Organization) {
-        for (org in organizationCollection){
-            if (org.id > organization.id) {
-                organizationCollection.remove(org)
-            }
-        }
-    }
+    fun removeGreater(organization: Organization) = organizationCollection.removeIf { it > organization}
 
-    fun removeLower(organization: Organization) {
-        for (org in organizationCollection){
-            if (org.id < organization.id) {
-                organizationCollection.remove(org)
-            }
-        }
-    }
+    fun removeLower(organization: Organization) = organizationCollection.removeIf { it < organization}
 
 
     override fun add(organization: Organization) {
