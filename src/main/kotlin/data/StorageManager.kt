1f306617212b14ac0040b8815cf9af3dfcf3e1bf
3fant <3fantimofey@gmail.com>
2026-02-18 14:48:47 +0300

    feat(data): add storage manager to control file writing\reading

---
+++ src/main/kotlin/data/StorageManager.kt
@@ -0,0 +1,48 @@
+package data
+
+import application.StorageGateway
+import domain.Organization
+import nl.adaptivity.xmlutil.serialization.XML
+import java.io.*
+import java.util.*
+import kotlin.collections.ArrayDeque
+
+class StorageManager: StorageGateway {
+    override fun downloadCollection(fileName: String): ArrayDeque<Organization> {
+        val file = File(fileName)
+        var res = ""
+        val sc = Scanner(file)
+
+        while(sc.hasNextLine()){
+            res += sc.nextLine()
+        }
+
+        val decode = XML.decodeFromString(OrganizationsContainer.serializer(), res)
+        val collection = decode
+            .organizations
+            .map(OrganizationDTO::toDomain)
+            .toList()
+
+        return ArrayDeque(collection)
+    }
+
+    override fun uploadCollection(collection: ArrayDeque<Organization>, fileName: String) {
+        val file = File(fileName)
+        val list = collection
+            .map(OrganizationDTO::toDto)
+            .toList()
+        val container = OrganizationsContainer(list)
+        val xml = XML { indentString = "    " }
+
+        try {
+            val content = xml.encodeToString(OrganizationsContainer.serializer(), container)
+            val fileWriter = FileWriter(file)
+
+            fileWriter.write(content)
+
+        }
+        catch (e: IOException) {
+            TODO("add ioexception handle")
+        }
+    }
+}
