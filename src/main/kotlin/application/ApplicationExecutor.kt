72766ae8262a6eb924b20f86fb2d5990aca9eabc
3fant <3fantimofey@gmail.com>
2026-02-20 20:20:05 +0300

    feat(app): register all commands into ApplicationExecutor and ScriptExecutor

--- src/main/kotlin/application/ApplicationExecutor.kt
+++ src/main/kotlin/application/ApplicationExecutor.kt
@@ -1,22 +1,23 @@
 package application
 
-import application.commands.Add
-import application.commands.ExecuteScript
-import application.commands.Show
+import application.commands.*
 import application.exceptions.WrongArgumentException
+import data.StorageManager
+import domain.Organization
 import java.io.EOFException
 
 class ApplicationExecutor(
     override val io: IOPort,
-    override val collectionManager: CollectionManager,
+    pathToFile: String
 ): Handler {
     override val invoker = CommandInvoker(this)
     override val inputReader = InputReader(this)
+    override val storageGateway: StorageManager = StorageManager()
 
+    private val collection = ArrayDeque<Organization>()
+    override val collectionManager = CollectionManager(collection)
 
-
-
-
+    override val logsManager = HistoryManager()
     override fun handleError(e: Exception) {
         io.printLine(e.message)
     }
@@ -25,17 +26,48 @@ class ApplicationExecutor(
         val add = Add(this)
         val executeScript = ExecuteScript(this)
         val show = Show(this)
-        invoker.registerCommand(show)
-        invoker.registerCommand(executeScript)
-        invoker.registerCommand(add)
+        val clear = Clear(this)
+        val countByType = CountByType(this)
+        val help = Help(this)
+        val info = Info(this)
+        val save = Save(this)
+        val sumOfEmployeesCount = SumOfEmployeesCount(this)
+        val countLessThanOfficialAddress = CountLessThanOfficialAddress(this)
+        val removeLower = RemoveLower(this)
+        val removeGreater = RemoveGreater(this)
+        val removeByID = RemoveByID(this)
+        val update = Update(this)
+        val exit = Exit(this)
+        val history = History(this)
+
+        with(invoker) {
+            registerCommand(show)
+            registerCommand(executeScript)
+            registerCommand(add)
+            registerCommand(clear)
+            registerCommand(countByType)
+            registerCommand(help)
+            registerCommand(info)
+            registerCommand(save)
+            registerCommand(sumOfEmployeesCount)
+            registerCommand(countLessThanOfficialAddress)
+            registerCommand(removeLower)
+            registerCommand(removeGreater)
+            registerCommand(removeByID)
+            registerCommand(update)
+            registerCommand(exit)
+            registerCommand(history)
+        }
+
         while (true) {
             try {
                 val line = io.readLine() ?: break
-                if (line.trim().lowercase() == "!exit") break
+
                 if (line.isBlank()) continue
 
                 try {
                     invoker.handleInput(line)
+                    logsManager.add(line)
                 } catch (e: WrongArgumentException){
                     io.printLine(e.message)
                     continue
@@ -49,5 +81,4 @@ class ApplicationExecutor(
             }
         }
     }
-
 }
--- src/main/kotlin/application/ScriptExecutor.kt
+++ src/main/kotlin/application/ScriptExecutor.kt
@@ -1,19 +1,20 @@
 package application
 
-import application.commands.Add
-import application.commands.ExecuteScript
+import application.commands.*
 
 class ScriptExecutor(
     val app: Handler,
-    val pathName: String
+    pathName: String
 ): Handler {
     init {
         println("Executing script $pathName")
     }
     override val io = ScriptReader(pathName)
     override val collectionManager = app.collectionManager
-    override val invoker: CommandInvoker = CommandInvoker(this)
-    override val inputReader: InputReader = InputReader(this)
+    override val invoker = CommandInvoker(this)
+    override val inputReader = InputReader(this)
+    override val storageGateway = app.storageGateway
+    override val logsManager = app.logsManager
 
     override fun handleError(e: Exception) {
         throw e
@@ -22,18 +23,45 @@ class ScriptExecutor(
     override fun run() {
         val add = Add(this)
         val executeScript = ExecuteScript(this)
-        invoker.registerCommand(executeScript)
-        invoker.registerCommand(add)
+        val show = Show(this)
+        val clear = Clear(this)
+        val countByType = CountByType(this)
+        val help = Help(this)
+        val info = Info(this)
+        val save = Save(this)
+        val sumOfEmployeesCount = SumOfEmployeesCount(this)
+        val countLessThanOfficialAddress = CountLessThanOfficialAddress(this)
+        val removeLower = RemoveLower(this)
+        val removeGreater = RemoveGreater(this)
+        val removeByID = RemoveByID(this)
+        val update = Update(this)
+        val exit = Exit(this)
+        val history = History(this)
+
+        with(invoker) {
+            registerCommand(show)
+            registerCommand(executeScript)
+            registerCommand(add)
+            registerCommand(clear)
+            registerCommand(countByType)
+            registerCommand(help)
+            registerCommand(info)
+            registerCommand(save)
+            registerCommand(sumOfEmployeesCount)
+            registerCommand(countLessThanOfficialAddress)
+            registerCommand(removeLower)
+            registerCommand(removeGreater)
+            registerCommand(removeByID)
+            registerCommand(update)
+            registerCommand(exit)
+            registerCommand(history)
+        }
+
         while (true) {
-            val line = io.readLine()
-            if (line == null) break
+            val line = io.readLine() ?: break
             if (line.isBlank()) continue
-
+            logsManager.add("script: $line")
             invoker.handleInput(line)
-
-            if (line.trim().lowercase() == "!exit") break
-
-
         }
     }
 }
