748f9551820ea8903679cf077f0377fb80332308
dmchq <gdima617@gmail.com>
2026-02-20 03:20:11 +0300

    feature(application): add script executor

---
+++ src/main/kotlin/application/ScriptExecutor.kt
@@ -0,0 +1,39 @@
+package application
+
+import application.commands.Add
+import application.commands.ExecuteScript
+
+class ScriptExecutor(
+    val app: Handler,
+    val pathName: String
+): Handler {
+    init {
+        println("Executing script $pathName")
+    }
+    override val io = ScriptReader(pathName)
+    override val collectionManager = app.collectionManager
+    override val invoker: CommandInvoker = CommandInvoker(io)
+    override val inputReader: InputReader = InputReader(this)
+
+    override fun handleError(e: Exception) {
+        throw e
+    }
+
+    override fun run() {
+        val add = Add(this)
+        val executeScript = ExecuteScript(this)
+        invoker.registerCommand(executeScript)
+        invoker.registerCommand(add)
+        while (true) {
+            val line = io.readLine()
+            if (line == null) break
+            if (line.isBlank()) continue
+
+            invoker.handleInput(line)
+
+            if (line.trim().lowercase() == "!exit") break
+
+
+        }
+    }
+}
