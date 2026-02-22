6a5dde487a788700f324ac20a5455f3ef42f5b4b
3fant <3fantimofey@gmail.com>
2026-02-22 17:17:45 +0300

    refactor(app): take out commands registering to invoker class

--- src/main/kotlin/application/CommandInvoker.kt
+++ src/main/kotlin/application/CommandInvoker.kt
@@ -1,12 +1,48 @@
 package application
 
-import application.commands.Command
+import application.commands.*
 
 class CommandInvoker(val app: Handler) {
     private val commands = mutableMapOf<String, Command>()
 
+    private val add = Add(app)
+    private val executeScript = ExecuteScript(app)
+    private val show = Show(app)
+    private val clear = Clear(app)
+    private val countByType = CountByType(app)
+    private val help = Help(app)
+    private val info = Info(app)
+    private val save = Save(app)
+    private val sumOfEmployeesCount = SumOfEmployeesCount(app)
+    private val countLessThanOfficialAddress = CountLessThanOfficialAddress(app)
+    private val removeLower = RemoveLower(app)
+    private val removeGreater = RemoveGreater(app)
+    private val removeByID = RemoveByID(app)
+    private val update = Update(app)
+    private val exit = Exit(app)
+    private val history = History(app)
+    
     fun registerCommand(command: Command) {
         commands[command.name] = command
+    }
+
+    init {
+        registerCommand(show)
+        registerCommand(executeScript)
+        registerCommand(add)
+        registerCommand(clear)
+        registerCommand(countByType)
+        registerCommand(help)
+        registerCommand(info)
+        registerCommand(save)
+        registerCommand(sumOfEmployeesCount)
+        registerCommand(countLessThanOfficialAddress)
+        registerCommand(removeLower)
+        registerCommand(removeGreater)
+        registerCommand(removeByID)
+        registerCommand(update)
+        registerCommand(exit)
+        registerCommand(history)
     }
 
     fun handleInput(input: String) {
