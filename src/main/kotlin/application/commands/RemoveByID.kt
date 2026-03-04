cb397dbf1bd741db03c0698c68f9f8fa0076011d
dmchq <gdima617@gmail.com>
2026-03-04 16:50:24 +0300

    fix(app): fix remove by id command name and description

--- src/main/kotlin/application/commands/RemoveByID.kt
+++ src/main/kotlin/application/commands/RemoveByID.kt
@@ -1,16 +1,25 @@
 package application.commands
 
 import application.Handler
+import application.exceptions.WrongArgumentException
 
 class RemoveByID  (
     override val app: Handler
 ): Command {
-    override val name = "remove_greater"
-    override val description = "Удаляет из коллекции все элементы, превышающие заданный"
+    override val name = "remove_by_id"
+    override val description = "Удаляет из коллекции элемент по ID"
 
     override fun execute(argument: String) {
         val collectionManager = app.collectionManager
-
-        collectionManager.removeById(argument.toInt())
+        try {
+            if (collectionManager.checkID(argument.toInt())) {
+                app.io.printLine("Элемента с таким ID не существует.")
+            } else {
+                collectionManager.removeById(argument.toInt())
+                app.io.printLine("Элемент с ID $argument удален.")
+            }
+        } catch (ex: NumberFormatException) {
+            throw WrongArgumentException("Введенный аргумент не является числом.")
+        }
     }
 }
