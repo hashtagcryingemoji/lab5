8d0ff870511c314981ccd17d42b31be54c024838
alllexey <alllexey.dev@gmail.com>
2026-03-14 11:07:10 +0300

    lil change

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,6 @@
 import application.ApplicationExecutor
 import ui.CliManager
 
-
 fun main(args: Array<String>) {
     val cliManager = CliManager()
     val app = ApplicationExecutor(cliManager, args.getOrNull(0) ?: "")
